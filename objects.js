0baf53f0785e6811cf4ce97be3359ae7b4bf79b8
rachel schest <rschest@gmail.com>
2017-11-01 16:30:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: "songTitle"};
 
-function updatePlaylist(){
+function updatePlaylist(var){
   var playlist = {Phil Ochs: "Heres to the state of Mississippi"}
   return playlist;
 }
