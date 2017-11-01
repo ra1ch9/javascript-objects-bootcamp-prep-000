2352fac13446aee5ccd91a853d22b4a0e2620ad0
rachel schest <rschest@gmail.com>
2017-11-01 16:49:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   updatePlaylist(playlist, 'Phil Ochs', 'Heres to the state of Mississippi');
 }
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+  delete playlist[artistName]
   return playlist;
 }
