39bc0ada75d2e718058e9daf8499ddd2252b98f9
rachel schest <rschest@gmail.com>
2017-11-01 16:38:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, obj, {[artistName]: songTitle});
+  return Object.assign({}, playlist, {[artistName]: songTitle});
   updatePlaylist(playlist, 'Phil Ochs', 'Heres to the state of Mississippi');
 
 
